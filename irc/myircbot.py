ec3c5912cfdae4f959688f8a5bee5f4d0b6c53a2
fccagou <me@fccagou.fr>
2020-03-24 21:31:57 +0100

    refact(myircbot): according to https://www.contributor-covenant.org/

--- irc/myircbot.py
+++ irc/myircbot.py
@@ -22,7 +22,7 @@ class IRC:
     irc_server="localhost"
     irc_port=6667
     irc_channel="#tuxoncloud"
-    my_master="fccagou"
+    my_love="fccagou"
     my_irc_nick="cagoubot"
     my_irc_user="cagoubot"
     my_irc_realname="Cagou Bot"
@@ -41,24 +41,24 @@ class IRC:
             'none': 0,
             'talk': 10,
             'join': 50,
-            'master_present': 100,
+            'love_present': 100,
      }
 
 
     def __init__(self, irc_server="localhost", irc_port=6667, irc_channel="#tuxoncloud",
-            my_master="fccagou", my_irc_nick="cagoubot", my_irc_user="cagoubot", my_irc_realname="Cagou Bot",
+            my_love="fccagou", my_irc_nick="cagoubot", my_irc_user="cagoubot", my_irc_realname="Cagou Bot",
             pyap_url=None):
         self.irc_server = irc_server
         self.irc_port = irc_port
         self.irc_channel = irc_channel
-        self.my_master = my_master
+        self.my_love = my_love
         self.my_irc_nick = my_irc_nick
         self.my_irc_user = my_irc_user
         self.my_irc_realname = my_irc_realname
-        self.is_my_master_connected = False
+        self.is_my_love_connected = False
         self.socket = None
         self.readbuffer = ""
-        self.nb_hello_master = 0
+        self.nb_hello_love = 0
         self.last_event = IRC.events_level['none']
         self.pyap_url = pyap_url
 
@@ -88,31 +88,31 @@ class IRC:
         self._send("PRIVMSG {0} :{1}".format(to, msg))
 
 
-    def talk_to_master(self, msg):
-        if self.is_my_master_connected:
-            self.send_mesg(self.my_master, msg)
+    def talk_to_love(self, msg):
+        if self.is_my_love_connected:
+            self.send_mesg(self.my_love, msg)
 
     def pong(self, msg):
         self._send("PONG {0}".format(msg))
 
-    def hello_master(self, tempo=0):
+    def hello_love(self, tempo=0):
         if tempo > 0:
-            info("Tempo before talking to master({0} s)...".format(tempo))
+            info("Tempo before talking to my love ({0} s)...".format(tempo))
             time.sleep(tempo)
 
-        info("Say hello to master {0}".format(self.my_master))
-        if self.nb_hello_master == 0:
-            self.talk_to_master("Hello {0}, how are U today ?".format(self.my_master, self.my_irc_nick))
-            self.nb_hello_master = self.nb_hello_master + 1
+        info("Say hello to my love {0}".format(self.my_love))
+        if self.nb_hello_love == 0:
+            self.talk_to_love("Hello {0}, how are U today ?".format(self.my_love, self.my_irc_nick))
+            self.nb_hello_love = self.nb_hello_love + 1
         else:
-            self.talk_to_master("Welcome back {0}, I missed U !".format(self.my_master, self.my_irc_nick))
+            self.talk_to_love("Welcome back {0}, I missed U !".format(self.my_love, self.my_irc_nick))
 
 
     def join(self):
         self._send("JOIN {0}".format(self.irc_channel))
         self.wait_until ("End of NAMES list")
-        if self.is_my_master_connected:
-            self.hello_master(tempo=3)
+        if self.is_my_love_connected:
+            self.hello_love(tempo=3)
 
     def wait_until(self, msg):
         info("Waiting server replies ({0})...".format(msg))
@@ -126,12 +126,12 @@ class IRC:
                     not_found = False
                     break
 
-                if not self.is_my_master_connected:
+                if not self.is_my_love_connected:
                     m = IRC.irc_names.match(line)
-                    self.is_my_master_connected = m and self.my_master in m[5]
+                    self.is_my_love_connected = m and self.my_love in m[5]
 
-                    if self.is_my_master_connected:
-                        self.notify_master_present()
+                    if self.is_my_love_connected:
+                        self.notify_love_present()
 
 
     def external_notify(self, event_type):
@@ -139,21 +139,21 @@ class IRC:
             self.last_event = event_type
             self.pyap_notify(self.last_event)
 
-    def notify_master_present(self):
-        self.external_notify('master_present')
+    def notify_love_present(self):
+        self.external_notify('love_present')
 
     def notify_new_join(self):
         self.external_notify('join')
 
     def notify_new_talk(self):
-        if 'master_present' == self.last_event or IRC.events_level['talk'] > IRC.events_level[self.last_event]:
+        if 'love_present' == self.last_event or IRC.events_level['talk'] > IRC.events_level[self.last_event]:
             self.external_notify('talk')
 
     def pyap_notify(self, status):
         if self.pyap_url is not None:
             status_list = {
                     'none': 'unknown',
-                    'master_present': 'security/ack ok',
+                    'love_present': 'security/ack ok',
                     'join': 'security/ack critical security',
                     'talk': 'security/ack warning security'
             }
@@ -179,18 +179,18 @@ class IRC:
 
                 if line.find("JOIN") > 0:
                     m = IRC.irc_join.match(line)
-                    if m and m[2] == self.my_master:
-                        info("My master is joining")
-                        self.is_my_master_connected = True
-                        self.notify_master_present ()
+                    if m and m[2] == self.my_love:
+                        info("My love is joining")
+                        self.is_my_love_connected = True
+                        self.notify_love_present ()
                     else:
                         self.notify_new_join ()
 
                 if line.find("QUIT") > 0:
                     m = IRC.irc_quit.match(line)
-                    if m and m[2] == self.my_master:
-                        info("Saaaad, My master quit {0}".format(m[4]))
-                        self.is_my_master_connected = False
+                    if m and m[2] == self.my_love:
+                        info("Saaaad, My love quit {0}".format(m[4]))
+                        self.is_my_love_connected = False
 
                 if line.find("PRIVMSG") > 0:
                     info("Who commands ?")
@@ -205,31 +205,31 @@ class IRC:
                        to = m[4]
                        from_msg = m[5]
 
-                       if from_user == self.my_master:
-                           # Message from master
-                           self.notify_master_present ()
+                       if from_user == self.my_love:
+                           # Message from love
+                           self.notify_love_present ()
                            if to == self.my_irc_nick:
                                # Is talking to me
-                               if not self.is_my_master_connected:
+                               if not self.is_my_love_connected:
                                    # TODO: improve the case
-                                   info("WARNING Got a msg from master when not connected ...")
-                                   self.is_my_master_connected = True
-                                   self.hello_master()
+                                   info("WARNING Got a msg from love when not connected ...")
+                                   self.is_my_love_connected = True
+                                   self.hello_love()
 
                                # Checking the question
                                if "clear notification" in from_msg:
                                    self.last_event = 'none'
-                                   self.notify_master_present ()
+                                   self.notify_love_present ()
                                else:
-                                   self.talk_to_master("Hum ... What do U mean by {0}".format(from_msg))
+                                   self.talk_to_love("Hum ... What do U mean by {0}".format(from_msg))
 
                        else:
                           self.notify_new_talk ()
                           # Someone else talking
                           if to == self.my_irc_nick:
                               # I fo not speak to unknown person.
-                              info("{0} is not my master !!".format(from_user))
-                              self.talk_to_master("ALERT {0} ".format(m[0]))
+                              info("{0} is not my love !!".format(from_user))
+                              self.talk_to_love("ALERT {0} ".format(m[0]))
 
 
 
@@ -275,8 +275,8 @@ if __name__ == '__main__':
                         help="irc port ({0})".format(IRC.irc_port))
     parser.add_argument('--ircchannel', '-c', default = IRC.irc_channel,
                         help="irc channel ({0})".format(IRC.irc_channel))
-    parser.add_argument('--mymaster', '-m', default = IRC.my_master,
-                        help="Got only one master ({0}) <3".format(IRC.my_master))
+    parser.add_argument('--mylove', '-m', default = IRC.my_love,
+                        help="Got only one love ({0}) <3".format(IRC.my_love))
     parser.add_argument('--myircnick', '-n', default = IRC.my_irc_nick,
                         help="My irc nick ({0})".format(IRC.my_irc_nick))
     parser.add_argument('--myircuser', '-u', default = IRC.my_irc_user,
@@ -292,7 +292,7 @@ if __name__ == '__main__':
     irc_bot = IRC( irc_server=args.server
             ,irc_port=args.port
             ,irc_channel=args.ircchannel
-            ,my_master=args.mymaster
+            ,my_love=args.mylove
             ,my_irc_nick=args.myircnick
             ,my_irc_user=args.myircuser
             ,my_irc_realname=args.myircrealname
