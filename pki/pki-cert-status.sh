77e54ebc9ba67600ac46c6ab7e8b2e9b61e58538
fccagou <me@fccagou.fr>
2024-08-17 22:35:27 +0200

    feat(pki): cert status allow serial as parameter

--- pki/pki-cert-status.sh
+++ pki/pki-cert-status.sh
@@ -3,21 +3,33 @@
 set -euo pipefail
 
 # Paramètres d'entrée
+CA_DIR="${CA_DIR:-myCA}"
 CERT_NAME=$1
+serial="unknown"
 
 if [ -z "${CERT_NAME}" ]; then
-    echo "Usage: $0 <certificate_name.crt>"
+    echo "Usage: $0 <certificate_name.crt|serial>"
     exit 1
 fi
 
 # Vérification si le fichier existe
 if [ ! -f "${CERT_NAME}" ]; then
-    echo "Le fichier ${CERT_NAME} n'existe pas." >&2
-    exit 1
+	if [ -f "${CA_DIR}"/newcerts/"${CERT_NAME}".pem ] ; then
+		serial="${CERT_NAME}"
+		CERT_NAME="${CA_DIR}"/newcerts/"${serial}".pem
+	else
+        echo "Le certificat ${CERT_NAME} n'existe pas." >&2
+        exit 1
+	fi
 fi
 
 # Vérification des dates de validité
-openssl x509 -in "${CERT_NAME}" -noout -dates || { echo "Erreur lors de la vérification des dates de validité pour ${CERT_NAME}." >&2; exit 1; }
+echo "-------"
+echo "serial=$serial"
+openssl x509 -in "${CERT_NAME}" -noout -subject -issuer -dates || {
+	echo "Erreur lors de la vérification des dates de validité pour ${CERT_NAME}." >&2
+	exit 1
+}
 
 # Message de fin
 echo "Vérification des dates de validité terminée pour ${CERT_NAME}."
