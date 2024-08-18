7594b01b42b2254d7ccdbab722d7c35f9389b6b7
fccagou <me@fccagou.fr>
2024-08-18 16:52:37 +0200

    add(pki): script template for new commandes

---
+++ pki/pki_script_template.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -euo pipefail
+
+
+usage () {
+	local cmd
+	cmd="${0##*/}"
+	cmd="${cmd//-/ }"
+    cmd="${cmd%%.sh}"
+    cat <<EOF_USAGE
+Usage: $cmd [--help|help]"
+
+Fonction pas encore implémentée
+
+EOF_USAGE
+}
+
+
+# Variables de configuration
+prefix="$(readlink -f "$(dirname "$0")")"
+confloader="$prefix"/pki-config-load.sh
+CA_DIR="${CA_DIR:-./myCA}"
+
+# Paramètres d'entrée
+if [ "$#" == "0" ]; then
+	usage
+    exit 1
+fi
+
+if [ "$1" == "--help" ] || [ "$1" == "help" ]; then
+	usage
+	exit 0
+fi
+
+[ -f "$confloader" ] || {
+    echo "Erreur, le fichier de chargement de configuration $confloader est absent." >&2
+	exit 1
+}
+
+source "$confloader"
+
+
+# Changer le code suivant.
+usage
+exit 1
+
