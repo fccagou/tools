3ba267447c546fb2f59d67da4d212bed0f639173
fccagou <me@fccagou.fr>
2024-08-18 17:54:52 +0200

    feat(pki): use config loader for cert sign

--- pki/pki-cert-sign.sh
+++ pki/pki-cert-sign.sh
@@ -12,6 +12,8 @@ usage () {
 
 
 # Variables de configuration
+prefix="$(readlink -f "$(dirname "$0")")"
+confloader="$prefix"/pki-config-load.sh
 CA_DIR="${CA_DIR:-./myCA}"
 CONFIG_FILE="${CA_DIR}/openssl.cnf"
 DAYS="${DAYS:-365}"
@@ -32,16 +34,45 @@ if [ "$1" == "--help" ] || [ "$1" == "help" ]; then
 	exit 0
 fi
 
+[ -f "$confloader" ] || {
+    echo "Erreur, le fichier de chargement de configuration $confloader est absent." >&2
+    exit 1
+}
+
+source "$confloader"
 CSR_FILENAME="$1"
-[ -f "${CSR_FILENAME}" ] || {
-	echo "Erreur, le fichier csr ${CSR_FILENAME} est absent." >&2
+
+certsdir="$(_config_get "${_ca}.certs")"
+csrrealname=""
+certname=""
+
+filenames=(
+	"${CSR_FILENAME}"
+	"${CSR_FILENAME}".csr
+	"$_requestdir"/"${CSR_FILENAME}"
+	"$_requestdir"/"${CSR_FILENAME}".csr
+)
+
+
+for f in "${filenames[@]}"; do
+	[ -f "$f" ] && {
+		csrrealname="$f"
+	    break
+	}
+done
+
+[ -n "$csrrealname" ]  || {
+	echo "Erreur, le fichier csr ${CSR_FILENAME} est introuvable." >&2
 	exit 1
 }
 
-if [ "${CSR_FILENAME%%.csr}" == "${CSR_FILENAME}" ]; then
-	CERT_NAME="${CSR_FILENAME}".pem
+
+tmp="${csrrealname##*/}"
+
+if [ "${tmp%%.csr}" == "${tmp}" ]; then
+	certname="${tmp}".pem
 else
-	CERT_NAME="${CSR_FILENAME%%.csr}".pem
+	certname="${tmp%%.csr}".pem
 fi
 
 
@@ -49,10 +80,12 @@ fi
 # Signature de la CSR par le CA
 openssl ca -days "${DAYS}" \
 	-config "${CONFIG_FILE}" \
-	-in "${CSR_FILENAME}" \
-	-out "${CERT_NAME}" \
-	|| { echo "Erreur lors de la signature du certificat ${CERT_NAME}." >&2; exit 1; }
+	-in "${csrrealname}" \
+	-out "$certsdir"/"$certname" \
+	|| { echo "Erreur lors de la signature du certificat ${csrrealname}." >&2; exit 1; }
 
+# On renomme pour ne plus le voir apparaître dans la liste.;
+mv "$csrrealname"{,.done}
 # Message de fin
-echo "Certificat ${CERT_NAME} signé avec succès."
+echo "Certificat $certsdir/$certname signé avec succès."
 
