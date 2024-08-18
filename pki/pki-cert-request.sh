c25c1d2f73bb8ddbe8996bcac7cca4bc02171df1
fccagou <me@fccagou.fr>
2024-08-18 17:20:35 +0200

    feat(pki): use config loader in cert request

--- pki/pki-cert-request.sh
+++ pki/pki-cert-request.sh
@@ -13,14 +13,14 @@ usage () {
 
 
 # Variables de configuration
+prefix="$(readlink -f "$(dirname "$0")")"
+confloader="$prefix"/pki-config-load.sh
+
 CA_DIR="${CA_DIR:-./myCA}"
 CONFIG_FILE="${CA_DIR}/openssl.cnf"
 DAYS="${DAYS:-365}"
 
 CERT_TYPE=server
-
-
-[ -f "${CONFIG_FILE}" ] || { echo "Erreur, le fichier de configuration ${CONFIG_FILE} est absent." >&2; exit 1; }
 
 # Paramètres d'entrée
 if [ "$#" == "0" ]; then
@@ -33,11 +33,18 @@ if [ "$1" == "--help" ] || [ "$1" == "help" ]; then
 	exit 0
 fi
 
+[ -f "$confloader" ] || {
+    echo "Erreur, le fichier de chargement de configuration $confloader est absent." >&2
+	exit 1
+}
+
+source "$confloader"
+
 CERT_NAME="${1// /-}"
 
 # Génération de la clé privée pour le certificat
 openssl genpkey -algorithm RSA \
-	-out "${CERT_NAME}".key \
+	-out "$_privatedir"/"${CERT_NAME}".key \
 	|| { echo "Erreur lors de la génération de la clé privée pour ${CERT_NAME}." >&2; exit 1; }
 
 # Création de la CSR (Certificate Signing Request)
@@ -45,10 +52,10 @@ openssl req --new \
 	-config "${CONFIG_FILE}" \
 	-extensions tls_"${CERT_TYPE}" \
 	-copy_extensions copyall \
-	-key "${CERT_NAME}".key \
-	-out "${CERT_NAME}".csr \
+	-key "$_privatedir"/"${CERT_NAME}".key \
+	-out "$_requestdir"/"${CERT_NAME}".csr \
 	|| { echo "Erreur lors de la création de la CSR pour ${CERT_NAME}." >&2; exit 1; }
 
 # Message de fin
-echo "Requtête de Certificat pour ${CERT_NAME}.csr créée avec succès."
+echo "Requtête de Certificat pour ${_requestdir}/${CERT_NAME}.csr créée avec succès."
 
