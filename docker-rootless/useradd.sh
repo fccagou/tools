12bb0183bcfd7735498362a8257853ae4a5c615f
fccagou <me@fccagou.fr>
2024-09-08 13:11:15 +0200

    fix(docker-rootless): hum ... to many bad code  not tested

--- docker-rootless/useradd.sh
+++ docker-rootless/useradd.sh
@@ -20,13 +20,13 @@ useradd --system --comment "$COMMENT" --create-home "$NEWUSER"
 
 userenv="$ROOTLESSDIR"/user.env
 
-[ -f "$usersnev ] || {
+[ -f "$userenv" ] || {
   # Ficher d'env
-  cat > "$userenv <<EOF
+  cat > "$userenv" <<EOF
 export XDG_RUNTIME_DIR=/run/user/"\$(id -u)"
-export DBUS_SESSION_BUS_ADDRESS=unix:path="${XDG_RUNTIME_DIR}"/bus
+export DBUS_SESSION_BUS_ADDRESS=unix:path="\${XDG_RUNTIME_DIR}"/bus
 export DOCKER_HOST=unix://"\${XDG_RUNTIME_DIR}"/docker.sock
-export PATH="$(pwd)"/bin:"\$PATH"
+export PATH="$ROOTLESSDIR"/bin:"\$PATH"
 EOF
 
   sha256sum "$userenv" >> sha256sum.txt
@@ -45,10 +45,13 @@ loginctl enable-linger "$NEWUSER"
 usersubuid="$(sort -t: -k2 /etc/subuid |tail -1 |awk -F: '{ print $2+$3+1 }')"
 usersubgid="$(sort -t: -k2 /etc/subgid |tail -1 |awk -F: '{ print $2+$3+1 }')"
 
+[ -n "$usersubuid" ] || usersubuid=100000
+[ -n "$usersubgid" ] || usersubgid=100000
+
 # Add subuid entry for NEWUSER
 grep -q "^${NEWUSER}:" /etc/subuid || echo "$NEWUSER:$usersubuid:65536" >> /etc/subuid
 # Add subgid entry for NEWUSER
 grep -q "^${NEWUSER}:" /etc/subgid || echo "$NEWUSER:$usersubgid:65536" >> /etc/subgid
 
-su - "$NEWUSER" -c "sleep 3; dockerd-rootless-setuptool.sh install && docker images"
+su - "$NEWUSER" -c "USER=$NEWUSER; sleep 3; dockerd-rootless-setuptool.sh install --skip-iptables && docker images"
 
