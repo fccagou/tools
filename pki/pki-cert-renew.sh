1b00e453a94cebe0b905fc9e98d848244d25aac3
fccagou <me@fccagou.fr>
2025-05-02 15:41:23 +0200

    feat(pki-cert-renew): use existing pki commands

--- pki/pki-cert-renew.sh
+++ pki/pki-cert-renew.sh
@@ -10,12 +10,6 @@ usage () {
     echo "Usage: $cmd <certificate_name|--help|help>"
 }
 
-# Variables de configuration
-CA_DIR="${CA_DIR:-./myCA}"
-CONFIG_FILE="${CA_DIR}/openssl.cnf"
-DAYS="${DAYS:-365}"
-
-[ -f "${CONFIG_FILE}" ] || { echo "Erreur, le fichier de configuration ${CONFIG_FILE} est absent." >&2; exit 1; }
 
 # Paramètres d'entrée
 if [ "$#" == "0" ]; then
@@ -29,18 +23,21 @@ if [ "$1" == "--help" ] || [ "$1" == "help" ]; then
 fi
 
 CERT_NAME="$1"
+prefix="$(dirname "$(readlink -f "$0")")"
+confloader="$prefix"/pki-config-load.sh
+
+source "$confloader"
+
+# Vérifier s'il y a déjà une requete en cours
 
 # Vérification si le fichier de clé privée existe
-if [ ! -f "${CERT_NAME}.key" ]; then
+if [ ! -f "$_privatedir"/"${CERT_NAME}.key" ]; then
     echo "La clé privée pour ${CERT_NAME} n'existe pas." >&2
     exit 1
 fi
 
-# Renouvellement: on génère une nouvelle CSR à partir de la clé existante
-openssl req -new -key "${CERT_NAME}".key -out "${CERT_NAME}".csr || { echo "Erreur lors de la création de la nouvelle CSR pour ${CERT_NAME}." >&2; exit 1; }
-
-# Signature de la nouvelle CSR par le CA pour renouveler le certificat
-openssl ca -config "${CONFIG_FILE}" -in "${CERT_NAME}".csr -out "${CERT_NAME}".crt -days "${DAYS}" || { echo "Erreur lors du renouvellement du certificat ${CERT_NAME}." >&2; exit 1; }
+"$prefix"/pki request new "$CERT_NAME"
+"$prefix"/pki cert sign "$CERT_NAME"
 
 # Message de fin
 echo "Certificat pour ${CERT_NAME} renouvelé avec succès."
